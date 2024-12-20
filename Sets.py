871fad4c82e9e461d136f8c2e318558b6c95418a
Uriyann <joshuabarotea06@gmail.com>
2024-12-21 00:50:24 +0800

    Sets

--- Sets.py
+++ Sets.py
@@ -1,2 +1,18 @@
 """Sets = Collection which is unordered, unindexed, and no duplicate values."""
 
+fruit = {"apple", "orange", "banana", "tea cup"}
+dishes = {"bowl", "plate", "cup", "tea cup"}
+
+
+# fruit.add("pineapple")                # Adding a new variable
+# fruit.remove("orange")                # Removing a certain variable
+# fruit.clear()                         # Clearing the entier sets
+# fruit.update(dishes)                  # Updating the set and randomizing it
+
+# dinner_table = fruit.union(dishes)    # Updating the set and randomizing it
+# print(dinner_table)
+# for x in dinner_table:
+#     print(x, end= ", ")
+
+# print(dishes.difference(fruit))       # Printing the difference of dishes to fruits
+# print(fruit.intersection(dishes))     # Printing the similarity from fruits to dishes
