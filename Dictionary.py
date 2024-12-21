bd637840e6aba2d15e5fd8778d7887449be2aa99
Uriyann <joshuabarotea06@gmail.com>
2024-12-21 16:52:53 +0800

    description

---
+++ Dictionary.py
@@ -0,0 +1,3 @@
+"""Dictionary = A changeable, unordered collection of unique key: 
+                value pairs. They are fast because they use hashing,
+                allowing us to access a value quickly"""
