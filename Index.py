2f584973d3b4db3cd5ea746707d7fe5903a86343
Uriyann <joshuabarotea06@gmail.com>
2024-12-23 12:20:20 +0800

    Index Operator

--- Index.py
+++ Index.py
@@ -1,9 +1,16 @@
 """Index Operator [] = Gives access to a sequence's element
                        (str, list, tuples)"""
 
-name = "joshie borrow"
+name = "joshie Borrow!"
 
-if(name[0].islower()):
-    name = name.capitalize()
+# if(name[0].islower()):            # It will determine if the variable is in lowercase letters
+#     name = name.capitalize()      # It will then capitalize the lowercase letters
 
-print(name)
+# first_name = name[0:6].upper()    # It will make the variable 'name' uppercase
+first_name = name[:6].upper()       # You dont need 0 to make it work as it is automatically starts from 0
+last_name = name[7:].lower()        # It will make the last name a lowercase letters
+last_char = name[-1]
+
+print(first_name)                   # Output: JOSHIE
+print(last_name)                    # Output: borrow!
+print(last_char)                    # Output: !
