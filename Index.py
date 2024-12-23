1537640007c2cd7fbe5deb1c57943aa39ba75364
Uriyann <joshuabarotea06@gmail.com>
2024-12-23 12:11:36 +0800

    Index Operator

---
+++ Index.py
@@ -0,0 +1,9 @@
+"""Index Operator [] = Gives access to a sequence's element
+                       (str, list, tuples)"""
+
+name = "joshie borrow"
+
+if(name[0].islower()):
+    name = name.capitalize()
+
+print(name)
