3302cdfb6013b2847f981612b9ce30fa817e9d70
Uriyann <joshuabarotea06@gmail.com>
2024-12-28 16:32:34 +0800

    Nested_Function_Calls

--- Nested_Function_Calls.py
+++ Nested_Function_Calls.py
@@ -1,1 +1,9 @@
 """Nested Function Calls = Are functions that we define inside other functions to directly access the variables and names defined in the enclosing function."""
+
+num = input("Enter a positive number: ")
+num = float(num)
+num = abs(num)
+num = round(num)
+print(num)                                                              # This way of method can have too many lines and it can be confusing
+
+print(round(abs(float(input("Enter another positive number: ")))))      # Efficient and less lines of code when nested
