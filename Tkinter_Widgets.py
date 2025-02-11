d44083a8f1fffe0584b544d3ff2633a0b5a05d8f
Uriyann <joshuabarotea06@gmail.com>
2025-02-11 22:52:42 +0800

    ttk

--- Tkinter_Widgets.py
+++ Tkinter_Widgets.py
@@ -1,10 +1,17 @@
 import tkinter as tk
+from tkinter import ttk
 
 main = tk.Tk()
 main.title("WIDGET TEST")
 main.geometry("800x500")
 
+label = tk.Label(master= main, text= "Wala Nang Peraaaa", font=("Times New Roman", 20))
+label.pack()
+
 text = tk.Text(master= main)
 text.pack()
+
+
+
 
 main.mainloop()
