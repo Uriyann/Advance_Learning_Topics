05861ee4eb3d634f8c23e0f444112a2cd4e1e372
Uriyann <joshuabarotea06@gmail.com>
2025-02-11 23:07:40 +0800

    done

--- Tkinter_Widgets.py
+++ Tkinter_Widgets.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 main = tk.Tk()
 main.title("WIDGET TEST")
-main.geometry("800x500")
+main.geometry("900x900")
 
 label = tk.Label(master= main, text= "Wala Nang Peraaaa", font=("Times New Roman", 20))
 label.pack()
@@ -14,7 +14,13 @@ text.pack()
 entry = ttk.Entry(master= main)
 entry.pack()
 
+another_label = tk.Label(master= main, text= "my label", font=("Times New Roman", 20))
+another_label.pack()
+
 button = ttk.Button(master= main, text= "A Button", command= main.destroy)
 button.pack()
+
+another_button = ttk.Button(master= main, text= "Hello Button", command= lambda: print("hello"))
+another_button.pack()
 
 main.mainloop()
