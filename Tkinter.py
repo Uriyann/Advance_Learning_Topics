e1bc5903abdfdb6bc104ccec5daf942656b76afb
Uriyann <joshuabarotea06@gmail.com>
2025-02-11 21:16:22 +0800

    frame

--- Tkinter.py
+++ Tkinter.py
@@ -1,15 +1,23 @@
 from tkinter import *
-import tkinter
-
+import tkinter as tk
+from PIL import Image, ImageTk
 
 main = Tk()
 main.title("TEST PROJECT")
 main.geometry("500x500")
 
-label_label= StringVar()
-
-label = Label(main, textvariable= label_label, font= ('Times New Roman', 20),
+label = Label(main, text= "MARK DE GUZMEN", font= ('Times New Roman', 20),
              bg= 'white', width= 24, height= 2)
 label.pack()
+
+
+image = Image.open(r"C:\Users\M S I\Downloads\IMG20250211142852.jpg")
+image = ImageTk.PhotoImage(image)
+
+image_label = tk.Label(main, image=image)
+image_label.pack()
+
+
+
 
 main.mainloop()
