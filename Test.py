031d06b04dd2d19e91b3e76831892236ae951bdc
Uriyann <joshuabarotea06@gmail.com>
2025-02-11 21:49:23 +0800

    tests

--- Test.py
+++ Test.py
@@ -1,18 +1,31 @@
+from tkinter import *
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
 
-# Create the main window
-parent = tk.Tk()
-parent.title("TEST PROJECT")
-parent.geometry("500x500")
+main = tk.Tk()
+main.title("TEST PROJECT")
+main.geometry("900x900")
 
-# Load and display an image 
-image = Image.open(r"C:\Users\M S I\Downloads\6d4fc21ede384265bf089310ab4e8a57.jpeg")
+label = Label(main, text= "ROCKY ROALD", font= ('Times New Roman', 20),
+             bg= 'white', width= 24, height= 2)
+label.pack()
+
+frame = ttk.Frame(main, width= 750, height= 750, borderwidth= 10, 
+                  relief= tk.GROOVE)
+frame.propagate(False)
+frame.pack()
+
+ext_label = ttk.Label(frame, text= "Good mood")
+ext_label.pack()
+
+image = Image.open(r"C:\Users\M S I\Downloads\b7e451f4-a649-4c8b-a46e-64c0cd3d6e32.jpg")
 image = ImageTk.PhotoImage(image)
 
-# Create a label to display the image
-image_label = tk.Label(parent, image=image)
+image_label = tk.Label(frame, width= 700, height= 700, image=image)
 image_label.pack()
 
-# Start the Tkinter event loop
-parent.mainloop()
+
+
+
+main.mainloop()
