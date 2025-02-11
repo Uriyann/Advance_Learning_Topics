7a5913ea0fa96ffe35b2dbc610db47c1a32c8be9
Uriyann <joshuabarotea06@gmail.com>
2025-02-11 20:22:37 +0800

    tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -1,6 +1,15 @@
+from tkinter import *
 import tkinter
-m = tkinter.Tk()
-'''
-widgets are added here
-'''
-m.mainloop()
+
+
+main = Tk()
+main.title("TEST PROJECT")
+main.geometry("500x500")
+
+label_label= StringVar()
+
+label = Label(main, textvariable= label_label, font= ('Times New Roman', 20),
+             bg= 'white', width= 24, height= 2)
+label.pack()
+
+main.mainloop()
